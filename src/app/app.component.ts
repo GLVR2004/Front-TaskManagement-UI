d6772cc16761577e27f7c534e014648c94fb09e6
LarzenVazquez <guillermolarzen@gmail.com>
2026-05-28 21:17:05 -0600

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { KanbanComponent } from './componets/kanban/kanban.component';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KanbanComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'TaskManagement-UI';
